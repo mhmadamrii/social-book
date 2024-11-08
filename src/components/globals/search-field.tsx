0530f9de557559ac82727bbee2721a9bad42e145
mhmadamrii <interceptorghost4@gmail.com>
2024-11-08 15:03:42 +0700

    fad;

---
+++ src/components/globals/search-field.tsx
@@ -0,0 +1,16 @@
+import { SearchIcon } from "lucide-react";
+import { Input } from "../ui/input";
+
+export function SearchField() {
+  const handleSubmit = () => {
+    console.log("Submitted");
+  };
+  return (
+    <form onSubmit={handleSubmit} method="GET" action="/search">
+      <div className="relative">
+        <Input name="q" placeholder="Search" className="pe-10" />
+        <SearchIcon className="absolute right-3 top-1/2 size-5 -translate-y-1/2 transform text-muted-foreground" />
+      </div>
+    </form>
+  );
+}
