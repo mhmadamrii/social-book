a5c3201590821ad38b92c3b6e74a5eaf74ceee2b
mhmadamrii <interceptorghost4@gmail.com>
2024-12-24 10:00:09 +0700

    fix: force refresh

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,6 +4,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { YourFeed } from "./_components/your-feed";
 import { FollowingFeed } from "./_components/following-feed";
 import { NoPostFound } from "~/components/globals/no-post-found";
+import { ForceRefresh } from "~/components/globals/force-refresh";
 
 export default async function Home() {
   const session = await auth();
@@ -11,6 +12,7 @@ export default async function Home() {
   return (
     <div className="w-full space-y-5">
       {session && <PostField currentUser={session?.current_user} />}
+      <ForceRefresh />
       <div className="w-full">
         <Tabs defaultValue="for-you">
           <TabsList className="bg-card dark:bg-slate-900">
